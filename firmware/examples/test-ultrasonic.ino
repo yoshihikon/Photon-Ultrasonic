768663fed4a23c3381435db848d32f9c8dc760fa
yoshihikon <yoshihiko@yBook.local>
2016-02-20 21:09:20 +0900

    update

---
+++ firmware/examples/test-ultrasonic.ino
@@ -0,0 +1,20 @@
+#include "Ultrasonic.h"
+
+Ultrasonic ultrasonic(7);
+
+int distance;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+    
+  long RangeInCentimeters;
+  RangeInCentimeters = ultrasonic.MeasureInCentimeters();
+  
+  delay(150);
+  
+  Serial.println(RangeInCentimeters, DEC);
+  
+}
